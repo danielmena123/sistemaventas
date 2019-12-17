1ac4d6208f7655a3e35675c9cf7da1de544766c7
Spunicher <54654920+Spunicher@users.noreply.github.com>
2019-12-17 16:54:38 -0600

    hoy si

--- sistema_ventas/src/main/java/com/proyecto/sistemaventas/entidades/Vendedor.java
+++ sistema_ventas/src/main/java/com/proyecto/sistemaventas/entidades/Vendedor.java
@@ -1,5 +1,82 @@
 package com.proyecto.sistemaventas.entidades;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Vendedor {
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+private int id;
+@Column
+private String nombre;
+@Column
+private String dui;
+@Column
+private String direccion;
+@Column
+private String telefono;
+
+public Vendedor(){}
+
+public Vendedor(int id, String nombre, String dui, String direccion, String telefono) {
+	super();
+	this.id = id;
+	this.nombre = nombre;
+	this.dui = dui;
+	this.direccion = direccion;
+	this.telefono = telefono;
+}
+
+public Vendedor(String nombre, String dui, String direccion, String telefono) {
+	this.nombre = nombre;
+	this.dui = dui;
+	this.direccion = direccion;
+	this.telefono = telefono;
+}
+
+public int getId() {
+	return id;
+}
+
+public void setId(int id) {
+	this.id = id;
+}
+
+public String getNombre() {
+	return nombre;
+}
+
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+
+public String getDui() {
+	return dui;
+}
+
+public void setDui(String dui) {
+	this.dui = dui;
+}
+
+public String getDireccion() {
+	return direccion;
+}
+
+public void setDireccion(String direccion) {
+	this.direccion = direccion;
+}
+
+public String getTelefono() {
+	return telefono;
+}
+
+public void setTelefono(String telefono) {
+	this.telefono = telefono;
+}
+
 
 }
--- sistema_ventas/src/main/java/com/proyecto/sistemaventas/entidades/Venta.java
+++ sistema_ventas/src/main/java/com/proyecto/sistemaventas/entidades/Venta.java
@@ -1,5 +1,117 @@
 package com.proyecto.sistemaventas.entidades;
 
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Venta {
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+private int id;
+@Column
+private double total;
+@Column
+private Date fechaventa;
+@ManyToOne(fetch = FetchType.LAZY)
+@JoinColumn(name = "IdVendedor")
+private Vendedor vendedor;
+
+@ManyToOne(fetch = FetchType.LAZY)
+@JoinColumn(name = "IdCliente")
+private Cliente cliente;
+
+@ManyToOne(fetch = FetchType.LAZY)
+@JoinColumn(name = "IdSucursal")
+private Sucursal sucursal;
+
+
+public Venta() {}
+
+
+public Venta(int id, double total, Date fechaventa, Vendedor vendedor, Cliente cliente, Sucursal sucursal) {
+	this.id = id;
+	this.total = total;
+	this.fechaventa = fechaventa;
+	this.vendedor = vendedor;
+	this.cliente = cliente;
+	this.sucursal = sucursal;
+}
+
+public Venta(double total, Date fechaventa, Vendedor vendedor, Cliente cliente, Sucursal sucursal) {
+	this.total = total;
+	this.fechaventa = fechaventa;
+	this.vendedor = vendedor;
+	this.cliente = cliente;
+	this.sucursal = sucursal;
+}
+
+
+public int getId() {
+	return id;
+}
+
+
+public void setId(int id) {
+	this.id = id;
+}
+
+
+public double getTotal() {
+	return total;
+}
+
+
+public void setTotal(double total) {
+	this.total = total;
+}
+
+
+public Date getFechaventa() {
+	return fechaventa;
+}
+
+
+public void setFechaventa(Date fechaventa) {
+	this.fechaventa = fechaventa;
+}
+
+
+public Vendedor getVendedor() {
+	return vendedor;
+}
+
+
+public void setVendedor(Vendedor vendedor) {
+	this.vendedor = vendedor;
+}
+
+
+public Cliente getCliente() {
+	return cliente;
+}
+
+
+public void setCliente(Cliente cliente) {
+	this.cliente = cliente;
+}
+
+
+public Sucursal getSucursal() {
+	return sucursal;
+}
+
+
+public void setSucursal(Sucursal sucursal) {
+	this.sucursal = sucursal;
+}
+
 
 }
