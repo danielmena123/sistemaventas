9c55505eecbfdac1923666003755aa3adbfee87b
frank <frank@192.168.56.1>
2019-12-17 15:54:52 -0600

    Insercion de Datos algunas Clases

--- sistema_ventas/src/main/java/com/proyecto/sistemaventas/entidades/Inventario.java
+++ sistema_ventas/src/main/java/com/proyecto/sistemaventas/entidades/Inventario.java
@@ -1,5 +1,52 @@
 package com.proyecto.sistemaventas.entidades;
 
-public class Inventario {
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
+@Entity
+public class Inventario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "IdSucursal")
+		private Sucursal sucursal;
+	
+	//Builders
+	
+	public Inventario() {}
+
+	public Inventario(int id, Sucursal sucursal) {
+		this.id = id;
+		this.sucursal = sucursal;
+	}
+	
+	public Inventario(Sucursal sucursal) {
+		this.sucursal = sucursal;
+	}
+	
+	//Getters && Setters
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Sucursal getSucursal() {
+		return sucursal;
+	}
+
+	public void setSucursal(Sucursal sucursal) {
+		this.sucursal = sucursal;
+	}
+	
+	
 }
--- sistema_ventas/src/main/java/com/proyecto/sistemaventas/entidades/Producto.java
+++ sistema_ventas/src/main/java/com/proyecto/sistemaventas/entidades/Producto.java
@@ -1,5 +1,49 @@
 package com.proyecto.sistemaventas.entidades;
 
-public class Producto {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
+@Entity
+public class Producto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	@Column(name = "NombreProducto")
+	private String nombre;
+	@Column(name = "PrecioProducto")
+	private double precio;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "IdProveedor")
+		private Proveedor proveedor;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "IdCategoria")
+		private Categoria categoria;
+	
+	//Builders
+	
+	public Producto() {}
+
+	public Producto(int id, String nombre, double precio, Proveedor proveedor, Categoria categoria) {
+		this.id = id;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.proveedor = proveedor;
+		this.categoria = categoria;
+	}
+	
+	public Producto(String nombre, double precio, Proveedor proveedor, Categoria categoria) {
+		this.nombre = nombre;
+		this.precio = precio;
+		this.proveedor = proveedor;
+		this.categoria = categoria;
+	}
+
+	//Getters && Setters
+	
 }
